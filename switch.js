21fb1c872029f0c0287e83611bb097c8d5322e8b
masudrana <masudranaacb@gmail.com>
2022-01-29 16:46:58 +0600

    basic switch case stractur

---
+++ switch.js
@@ -0,0 +1,17 @@
+var color='red';
+
+switch(color){
+    case 'green':
+            console.log("thats color is maching green to green");
+        break;
+    case 'red':
+        console.log("thats color is machin red to red");
+        break;
+    case 'white':
+        console.log("color is machis white to white");
+        break;
+
+    default:
+        console.log(' is black' );
+    
+}
